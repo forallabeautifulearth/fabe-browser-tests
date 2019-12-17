f2d6fd1032b5811197c49867b34e2a182806b71c
Hayden Hudson <hhudson@insum.ca>
2019-12-16 22:11:04 -0500

    latest for p180

--- cypress/integration/admin/app201.p180.admin.js
+++ cypress/integration/admin/app201.p180.admin.js
@@ -8,14 +8,25 @@ context("test p180", () => {
   const deleteSeq = Cypress._.random(1e6, 2e6).toString();
   // it is a good idea to randomize each item since we use shared backed
   const deleteName = `Delete me (test ${deleteSeq})`;
-  const page_number = 20;
-  const page_name = "actions";
+  const page_number = 180;
+  const page_name = "species";
   it("create species", () => {
-    cy.adminLoginSetup(180);
-    cy.openCreateModal(deleteName);
+    cy.adminLoginSetup(page_number);
+    //cy.verifyNotThere(deleteName);
+    cy.openCreateModal();
+    cy.get("iframe").then(function($iframe) {
+      const $body = $iframe.contents().find("body");
+      const cyDom1 = cy.wrap($body);
+      cyDom1.find("#P181_SPECIES_NAME").type(deleteName);
+      const cyDom2 = cy.wrap($body);
+      cyDom2.find("#P181_SPECIES_DESCRIPTION").type(deleteName);
+      cy.uploadImg($body, page_number + 1);
+      cy.submitModal($body);
+    });
     getIframeDom()
-      .find("#P181_SPECIES_NAME")
-      .type(deleteSeq)
-      .should("have.value", deleteSeq);
+      .find("[data-cy=cancelButton]")
+      .click();
+    cy.get("#species_search_field").type("Delete {enter}");
+    cy.confirmItem(deleteName, page_name);
   });
 });
