d2ac50049131bd414b3b2ec9f72bbe1e96ceaa13
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-12-19 09:52:22 -0500

    update new row test

--- cypress/integration/admin/app201.p210.admin.js
+++ cypress/integration/admin/app201.p210.admin.js
@@ -1,11 +1,22 @@
 context("testing mantras page", () => {
   it("add a row to ig", () => {
     cy.adminLoginSetup(210);
+
+    // cy.contains("button", "Reset").click();
+
     cy.get("[data-action=selection-add-row]").click();
-    cy.get(".is-focused")
-      .dblclick()
-      .type("test");
-    cy.get("td#mantras_ig_grid_vc_cur").type("Delete me test");
+
+    const label = "Test action " + Cypress._.random(1e6);
+
+    cy.focused()
+      .should("have.class", "apex-item-text")
+      .type(label)
+      .blur();
+
+    cy.contains("button", "Save").click();
+    cy.get("#mantras_ig_toolbar_search_field").type(label + "{enter}");
+    // cy.pause();
+    // cy.get("td#mantras_ig_grid_vc_cur").type("Delete me test");
     //.should("have.value", "Delete me test")
   });
 });
