d0261524354bbd0a4192b43c0185fe43bc2d14ae
Hayden Hudson <hhudson@insum.ca>
2020-01-02 09:39:52 -0500

    got rid of everything on p9996 too

--- cypress/integration/app/app200.p9996.js
+++ cypress/integration/app/app200.p9996.js
@@ -1,10 +1,10 @@
 context("App 200 page 9996", () => {
   it("remembers me", () => {
     cy.userLoginSetup(9996);
-    const pUserEmail = Cypress.env("userEmail");
-    cy.getCy("signup_email").type(pUserEmail);
-    cy.getCy("submitButton").click();
-    cy.url().should("contain", ":9999:");
-    cy.get(".t-Alert-title").should("be.visible");
+    //const pUserEmail = Cypress.env("userEmail");
+    //cy.getCy("signup_email").type(pUserEmail);
+    //cy.getCy("submitButton").click();
+    //cy.url().should("contain", ":9999:");
+    //cy.get(".t-Alert-title").should("be.visible");
   });
 });
