872a4de475243752e3fda1842401f256715e5ab3
Hayden Hudson <hhudson@insum.ca>
2020-07-16 20:11:58 -0400

    added a period per requirement (#25)

--- cypress/integration/admin/app201.p20.admin.js
+++ cypress/integration/admin/app201.p20.admin.js
@@ -7,7 +7,7 @@ import {
 context("App 201 (Admin) p20 - Actions", () => {
   const deleteSeq = Cypress._.random(1e6, 2e6).toString();
   // it is a good idea to randomize each item since we use shared backed
-  const deleteName = `Delete me (test ${deleteSeq})`;
+  const deleteName = `Delete me (test ${deleteSeq}).`;
   const page_number = 20;
   const page_name = "actions";
 
